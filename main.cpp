772206f91b5e3e7e7c88de835d102caa4f9d5fe5
balexandruvalentin <95694576+balexandruvalentin@users.noreply.github.com>
2021-12-07 17:50:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ void initOpr(stivaOpr &s) {
     s.varf = NULL;
     s.nrElemente = 0;
 }
-    
+
 char* popOpr(stivaOpr &s) {
     char *val;
     strcpy(val, s.varf->val);
@@ -91,8 +91,9 @@ void initOpd(stivaOpd &s) {
     s.nrElemente = 0;
 }
 
-anod* popOpd(stivaOpd &s) {
-    anod * nod = s.varf -> nod;
+char* popOpd(stivaOpd &s) {
+    char *val;
+    strcpy(val, s.varf -> nod -> val);
 
     nodOpd* varf_nou = s.varf -> urm;
 
@@ -100,17 +101,20 @@ anod* popOpd(stivaOpd &s) {
 
     s.varf = varf_nou;
     s.nrElemente--;
-    return nod;
+    return val;
 }
 
-void pushOpd(stivaOpd &s, anod* nod) {
+void pushOpd(stivaOpd &s, char* val) {
 
     if(s.nrElemente == 0) {
 
         s.nrElemente = 1;
         s.varf = new nodOpd;
 
-        s.varf -> nod = nod;
+        s.nrElemente++;
+
+        //s.varf -> val = elem
+        strcpy(s.varf -> nod -> val, val);
         s.varf -> urm = NULL;
     }
 
@@ -120,21 +124,49 @@ void pushOpd(stivaOpd &s, anod* nod) {
 
         s.nrElemente++;
 
-        //nod_nou -> val = elem;
-        nod_nou -> nod = nod;
+        //nod_nou -> val = elem
+        strcpy(nod_nou -> nod -> val, val);
         nod_nou -> urm = s.varf;
         s.varf = nod_nou;
     }
 
 }
 
+//de verificare
+void printOpr(stivaOpr Opr) {
+    int k = 0;
+    while(k < Opr.nrElemente) {
+            cout << Opr.varf -> val << " ";
+            Opr.varf = Opr.varf -> urm;
+            k++;
+    }
+    cout << endl;
+}
+
+void printOpd(stivaOpd Opd) {
+    int k = 0;
+    while(k < Opd.nrElemente) {
+            cout << Opd.varf -> nod -> val << " ";
+            Opr.varf = Opr.varf -> urm;
+            k++;
+    }
+    cout << endl;
+}
+
 //--------------- alte functii ------------//
 
-bool isSeparator(char c){
-
+bool esteSeparator(char c) {
     char separatori[20] = "()+-*/^=<>#";
     if(strchr(separatori, c))
         return true;
+    return false;
+}
+
+bool esteFunctie(char s[]) {
+    char functii[30][7] = {"sin", "cos", "abs", "exp", "ln", "sqrt"};
+    for(int i=0; i<6; i++)
+        if(!strcmp(functii[i],s))
+            return true;
     return false;
 }
 
@@ -142,16 +174,16 @@ void extragereCuv(char tokenArr[][100], char exp[]){
     char cuv[N];
     int len = strlen(exp);
 
-    for(int j=0,i=0; j<len; j++){
+    for(int j=0,i=0; j<=len; j++){
 
-        if(isSeparator(exp[j])){
+        if(esteSeparator(exp[j])){
 
             int n=0;
             for(int p=i; p<j; p++){
                 cuv[n++] = exp[p];
             }
             cuv[n] = '\0';
-           
+
              if(i != j)
                 strcpy(tokenArr[E.ArrLen++], cuv);
 
@@ -162,21 +194,49 @@ void extragereCuv(char tokenArr[][100], char exp[]){
             E.ArrLen++;
         }
     }
+
+    E.ArrLen--;
 }
 
+void formareStive(expr E, stivaOpr Opr, stivaOpd Opd) {
+    strcpy(Opr.varf -> val, "(");
+    Opr.nrElemente = 1;
+    int i=0;
+    while (i < E.ArrLen) {
+        if(esteSeparator(E.tokenArr[i][0]) || esteFunctie(E.tokenArr[i])) {
+            pushOpr(Opr,E.tokenArr[i]);
+            cout<<E.tokenArr[i]<<" ";
+        }
+        else {
+            pushOpd(Opd,E.tokenArr[i]);
+            cout<<E.tokenArr[i];
+        }
+    i++;
+    }
+    pushOpd(Opd,")");
+}
 int main()
 {
-    /*
+
     char exp[N];
     cin.getline(exp, N-1);
 
     extragereCuv(E.tokenArr, exp);
 
+    /*
     for(int i=0; i<E.ArrLen; i++){
         cout << endl << E.tokenArr[i];
     }
     */
-    
+
+    stivaOpr Opr;
+    stivaOpd Opd;
+
+    formareStive(E, Opr, Opd);
+
+    printOpr(Opr);
+    printOpd(Opd);
+
     return 0;
 }
 
