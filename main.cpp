c88fd59363b5b0ce505379a39850ca6f9daf0858
Stefan Leustean <73607989+stefali1-dev@users.noreply.github.com>
2021-12-07 19:23:32 +0200

    Update main.cpp
    Modificari:
    - am schimbat ca pop-ul si push-ul in Opd sa fie de tipul anod
    - am modificat functia de verificare printOpd
    - mici modificari ulterioare in functia formareStive

--- main.cpp
+++ main.cpp
@@ -6,53 +6,59 @@ using namespace std;
 
 // ------------ structuri ----------//
 
-struct anod{
+struct anod
+{
 
     char val[100];
     struct nod *st;
     struct nod *dr;
 };
 
-struct nodOpr {
+struct nodOpr
+{
     char val[100];
     nodOpr * urm;
 };
 
-struct nodOpd {
+struct nodOpd
+{
     anod * nod;
     nodOpd * urm;
 };
 
-struct stivaOpr{
+struct stivaOpr
+{
     nodOpr* varf;
     int nrElemente;
-}Opr;
+} Opr;
 
-struct stivaOpd{
+struct stivaOpd
+{
     nodOpd* varf;
     int nrElemente = 0;
-}Opd;
+} Opd;
 
-struct expr{
+struct expr
+{
 
     char tokenArr[N][100];
     int ArrLen = 0;
 
-}E;
+} E;
 
 //-------------- functii stiva --------------//
 
-void initOpr(stivaOpr &s) {
+void initOpr(stivaOpr &s)
+{
     s.varf = NULL;
     s.nrElemente = 0;
 }
 
-char* popOpr(stivaOpr &s) {
+char* popOpr(stivaOpr &s)
+{
     char *val;
     strcpy(val, s.varf->val);
-
     nodOpr* varf_nou = s.varf -> urm;
-
     delete s.varf;
 
     s.varf = varf_nou;
@@ -61,9 +67,11 @@ char* popOpr(stivaOpr &s) {
 }
 
 
-void pushOpr(stivaOpr &s, char* val) {
+void pushOpr(stivaOpr &s, char* val)
+{
 
-    if(s.nrElemente == 0) {
+    if(s.nrElemente == 0)
+    {
 
         s.nrElemente = 1;
         s.varf = new nodOpr;
@@ -72,7 +80,8 @@ void pushOpr(stivaOpr &s, char* val) {
         s.varf -> urm = NULL;
     }
 
-    else {
+    else
+    {
         nodOpr* nod_nou;
         nod_nou = new nodOpr;
 
@@ -86,83 +95,87 @@ void pushOpr(stivaOpr &s, char* val) {
 
 }
 
-void initOpd(stivaOpd &s) {
+void initOpd(stivaOpd &s)
+{
     s.varf = NULL;
     s.nrElemente = 0;
 }
 
-char* popOpd(stivaOpd &s) {
-    char *val;
-    strcpy(val, s.varf -> nod -> val);
-
+anod* popOpd(stivaOpd &s)
+{
+    anod *nod;
+    nod = s.varf -> nod;
     nodOpd* varf_nou = s.varf -> urm;
-
     delete s.varf;
 
     s.varf = varf_nou;
     s.nrElemente--;
-    return val;
+    return nod;
 }
 
-void pushOpd(stivaOpd &s, char* val) {
+void pushOpd(stivaOpd &s, anod* nod)
+{
 
-    if(s.nrElemente == 0) {
-
+    if(s.nrElemente == 0)
+    {
         s.nrElemente = 1;
         s.varf = new nodOpd;
-
         s.nrElemente++;
 
-        //s.varf -> val = elem
-        strcpy(s.varf -> nod -> val, val);
+        s.varf -> nod = nod;
         s.varf -> urm = NULL;
     }
 
-    else {
+    else
+    {
         nodOpd* nod_nou;
         nod_nou = new nodOpd;
-
         s.nrElemente++;
 
-        //nod_nou -> val = elem
-        strcpy(nod_nou -> nod -> val, val);
+        nod_nou -> nod = nod;
         nod_nou -> urm = s.varf;
         s.varf = nod_nou;
     }
 
 }
 
-//de verificare
-void printOpr(stivaOpr Opr) {
+//-----------functii de verificare -----------//
+
+void printOpr(stivaOpr Opr)
+{
     int k = 0;
-    while(k < Opr.nrElemente) {
-            cout << Opr.varf -> val << " ";
-            Opr.varf = Opr.varf -> urm;
-            k++;
+    while(k < Opr.nrElemente)
+    {
+        cout << Opr.varf -> val << endl;
+        Opr.varf = Opr.varf -> urm;
+        k++;
     }
     cout << endl;
 }
 
-void printOpd(stivaOpd Opd) {
-    int k = 0;
-    while(k < Opd.nrElemente) {
-            cout << Opd.varf -> nod -> val << " ";
-            Opr.varf = Opr.varf -> urm;
-            k++;
+void printOpd(stivaOpd Opd)
+{
+    nodOpd *nod_nou;
+    nod_nou = Opd.varf;
+    while(nod_nou)
+    {
+        cout << nod_nou ->nod ->val << endl;
+        nod_nou = nod_nou ->urm;
     }
-    cout << endl;
 }
 
 //--------------- alte functii ------------//
 
-bool esteSeparator(char c) {
+bool esteSeparator(char c)
+{
     char separatori[20] = "()+-*/^=<>#";
     if(strchr(separatori, c))
         return true;
     return false;
 }
 
-bool esteFunctie(char s[]) {
+bool esteFunctie(char s[])
+{
     char functii[30][7] = {"sin", "cos", "abs", "exp", "ln", "sqrt"};
     for(int i=0; i<6; i++)
         if(!strcmp(functii[i],s))
@@ -170,21 +183,25 @@ bool esteFunctie(char s[]) {
     return false;
 }
 
-void extragereCuv(char tokenArr[][100], char exp[]){
+void extragereCuv(char tokenArr[][100], char exp[])
+{
     char cuv[N];
     int len = strlen(exp);
 
-    for(int j=0,i=0; j<=len; j++){
+    for(int j=0,i=0; j<=len; j++)
+    {
 
-        if(esteSeparator(exp[j])){
+        if(esteSeparator(exp[j]))
+        {
 
             int n=0;
-            for(int p=i; p<j; p++){
+            for(int p=i; p<j; p++)
+            {
                 cuv[n++] = exp[p];
             }
             cuv[n] = '\0';
 
-             if(i != j)
+            if(i != j)
                 strcpy(tokenArr[E.ArrLen++], cuv);
 
             i = j+1;
@@ -198,23 +215,31 @@ void extragereCuv(char tokenArr[][100], char exp[]){
     E.ArrLen--;
 }
 
-void formareStive(expr E, stivaOpr Opr, stivaOpd Opd) {
-    strcpy(Opr.varf -> val, "(");
-    Opr.nrElemente = 1;
+
+void formareStive(expr E)
+{
+    initOpr(Opr); initOpd(Opd);
+    pushOpr(Opr, "(");
+
     int i=0;
-    while (i < E.ArrLen) {
-        if(esteSeparator(E.tokenArr[i][0]) || esteFunctie(E.tokenArr[i])) {
+    while (i < E.ArrLen)
+    {
+        if(esteSeparator(E.tokenArr[i][0]) || esteFunctie(E.tokenArr[i]))
+        {
             pushOpr(Opr,E.tokenArr[i]);
-            cout<<E.tokenArr[i]<<" ";
         }
-        else {
-            pushOpd(Opd,E.tokenArr[i]);
-            cout<<E.tokenArr[i];
+        else
+        {
+            anod *nod_nou;
+            nod_nou = new anod;
+            strcpy(nod_nou ->val, E.tokenArr[i]);
+            pushOpd(Opd, nod_nou);
         }
-    i++;
+        i++;
     }
-    pushOpd(Opd,")");
+    pushOpr(Opr, ")");
 }
+
 int main()
 {
 
@@ -223,20 +248,32 @@ int main()
 
     extragereCuv(E.tokenArr, exp);
 
-    /*
-    for(int i=0; i<E.ArrLen; i++){
-        cout << endl << E.tokenArr[i];
-    }
+
+    //for(int i=0; i<E.ArrLen; i++){
+    //    cout << endl << E.tokenArr[i];
+    //}
+
+    
+    formareStive(E);
+    cout << "Operatorii:\n";
+    printOpr(Opr);
+    cout << "\nOperanzii:\n";
+    printOpd(Opd);
+
+    /* Testare Opd
+    anod *nod_nou;
+    nod_nou = new anod;
+    strcpy(nod_nou ->val, "sal");
+    initOpd(Opd);
+    pushOpd(Opd, nod_nou);
+    printOpd(Opd);
     */
 
-    stivaOpr Opr;
-    stivaOpd Opd;
-
-    formareStive(E, Opr, Opd);
-
+    /* Testare Opr
+    initOpr(Opr);
+    pushOpr(Opr, "salll");
     printOpr(Opr);
-    printOpd(Opd);
+    */
 
     return 0;
 }
-
